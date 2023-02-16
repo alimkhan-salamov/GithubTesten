bc777046fad4a75a4071882ee163e5239e73f10a
Alimkhan8195 <alimkhan.salamov@gmail.com>
2023-02-16 15:40:50 +0100

    Hello World op label plaatsen dmv klik op button

--- Github/MainWindow.xaml.cs
+++ Github/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Github
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lblResult.Content = "Hello World";
+        }
     }
 }
